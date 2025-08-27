217f00dea3829aeec9c31865a6c4fad449641432
mustakhusain999 <mustakhusain999@gmail.com>
2025-08-27 21:20:39 +0530

    Add files via upload

--- Task 2  Assignment 5.py
+++ Task 2  Assignment 5.py
@@ -1,7 +1,7 @@
 # Task 2: Demonstrate List Slicing
 
 # Step1: Create a list of numbers
-numbers = [1,2,3,4,5,6,7,8,9,10]
+numbers = list(range(1, 11))
 
 # Step2: Extracting the first five elements from the list
 extracted = numbers[0:5]
